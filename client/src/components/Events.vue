8945f2103987de3d0917151c156223b7cd899dfe
Giovane Costa <gigio.coder@gmail.com>
2019-05-06 23:32:41 -0300

    Creates components files

---
+++ client/src/components/Events.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link to="/users">Users</router-link>
+    <p>
+      Event List
+    </p>
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+  export default {
+    data() {
+      return {};
+    }
+  }
+</script>
---
+++ client/src/components/users/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Users List</h1>
+    <p>
+      <ul>
+        <li v-for="user in userList" :key="user.id">
+          {{ user.name }}
+        </li>
+      </ul>
+    </p>
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+
+  import { mapState } from 'vuex';
+
+  export default {
+    data() {
+      return {};
+    },
+    computed: {
+      ...mapState(['userList']),
+    },
+    beforeCreate() {
+      this.$store.dispatch('fetchUserList');
+    },
+  }
+</script>
