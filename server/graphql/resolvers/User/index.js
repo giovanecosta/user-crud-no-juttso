c233db9d67dc278a7658756916522801303c67e4
Giovane Costa <gigio.coder@gmail.com>
2019-05-07 08:22:37 -0300

    Events being saved on User operations

--- server/graphql/resolvers/User/index.js
+++ server/graphql/resolvers/User/index.js
@@ -1,12 +1,30 @@
-// The User schema.
 import User from "../../../models/User";
+import Event from "../../../models/Event";
+
+async function registerEvent(type, _author) {
+  const newEvent = new Event({ type, _author, time: new Date() });
+
+  return new Promise((resolve, reject) => {
+    newEvent.save(( err ) => {
+      err ? reject(err) : resolve(newEvent);
+    });
+  });
+}
 
 export default {
   Query: {
     user: (root, args) => {
       return new Promise((resolve, reject) => {
         User.findOne(args).exec((err, res) => {
-          err ? reject(err) : resolve(res);
+          if (err) {
+            reject(err);
+          } else {
+            registerEvent('view', args._id);
+
+            // Users will not have reference for view events for now. Event still have User id;
+
+            resolve(res);
+          }
         });
       });
     },
@@ -25,16 +43,32 @@ export default {
       const newUser = new User({ id, name, email, phone, password });
 
       return new Promise((resolve, reject) => {
-        newUser.save((err, res) => {
-          err ? reject(err) : resolve(res);
+        newUser.save(async (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            const newEvent = await registerEvent('create', newUser._id);
+
+            newUser.events.push(newEvent);
+            newUser.save();
+
+            resolve(res);
+          }
         });
       });
     },
     editUser: (root, { _id, name, email, phone, password }) => {
       return new Promise((resolve, reject) => {
         User.findOneAndUpdate({ _id }, { $set: { name, email, phone, password } }).exec(
-          (err, res) => {
-            err ? reject(err) : resolve(res);
+          async (err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              registerEvent('update', _id);
+
+              // Users will not have reference for update events for now. Event still have User id;
+              resolve(res);
+            }
           }
         );
       });
@@ -42,7 +76,13 @@ export default {
     deleteUser: (root, args) => {
       return new Promise((resolve, reject) => {
         User.findOneAndRemove(args).exec((err, res) => {
-          err ? reject(err) : resolve(res);
+          if (err) {
+            reject(err);
+          } else {
+            registerEvent('delete', args._id);
+
+            resolve(res);
+          }
         });
       });
     }
