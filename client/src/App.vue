dc05dc209fb8374ee48b2c87cbb0cd6cd03daa32
Giovane Costa <gigio.coder@gmail.com>
2019-05-06 23:33:12 -0300

    Configure and put things together

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
